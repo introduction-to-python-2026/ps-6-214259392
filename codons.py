a4e9b597912da3836ab35051d5c15f6c3cb78779
214259392 <tiferetw@gmail.com>
2025-11-27 23:03:20 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
+    path = "data/codons.txt"
+    file = open(path)
+    rows = file.readlines()
+    file.close()
+    codon_dict = {}
+    for row in rows:
+     cells = row.strip().split('\t')
+     codon = cells [0]
+     abbreviation = cells[2]
+     codon_dict[codon] = abbreviation
+    print (codon_dict)  
 
